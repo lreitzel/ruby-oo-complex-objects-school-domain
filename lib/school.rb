3d1090ae749634401355fada9b17ca4d9ba3d19b
Lydia <lreitzel@gmail.com>
2021-01-28 20:53:55 -0800

    commit tree 695e99e8010fa0879cbf2ffff8b677e4e4b07e66

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
 # code here!
+class School
+    attr_accessor :name, :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(student, grade)
+        roster[grade] ||= []
+        roster[grade] << student
+    end
+
+    def grade(year)
+        roster[year]
+    end
+
+    def sort
+        in_order = {}
+        roster.each do |grade, students|
+            in_order[grade] = students.sort
+        end
+        in_order
+    end
+end
